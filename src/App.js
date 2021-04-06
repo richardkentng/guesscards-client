4267ed7e2b19b58f35abe3797dc5b2cf0e874b88
Richard Ng <richardkentng@gmail.com>
2021-04-06 14:17:29 -0700

    adds Switch and Route for signup component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import {Switch, Route} from 'react-router-dom'
+
+import Signup from './pages/Signup'
 
 
 function App() {
   return (
     <div>
       hi there. im the app component
+      <Switch>
+        <Route path="/signup" component={Signup}/>
+      </Switch>
     </div>
   );
 }
