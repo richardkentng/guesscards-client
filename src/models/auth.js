f68fa4f33c3e155b57e8662dae19704c10411ff4
Richard Ng <richardkentng@gmail.com>
2021-04-06 19:18:58 -0700

    creates authModel for signup

---
+++ src/models/auth.js
@@ -0,0 +1,19 @@
+    const urlApi = "http://localhost:4000"
+
+    class AuthModel {
+        static signup = (body) => {
+            const fetchOptions = {
+                method: "POST",
+                headers: {"Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            }
+            let request = fetch(`${urlApi}/signup`, fetchOptions).then(res => res.json())
+            return request
+        }
+
+        static sayHi = () => {
+            console.log('hi!');
+        }
+    }
+
+    export default AuthModel
