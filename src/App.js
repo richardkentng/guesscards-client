9d62fb2511719bfc4ca01562aa67edcfb42fdce9
Richard Ng <richardkentng@gmail.com>
2021-04-06 19:25:25 -0700

    creates and renders empty SetContainer component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import {Switch, Route} from 'react-router-dom'
 
 import Signup from './pages/Signup'
-
+import SetContainer from './pages/SetContainer'
 
 function App() {
   return (
     <div>
-      hi there. im the app component
       <Switch>
         <Route path="/signup" component={Signup}/>
+        <Route path="/sets" component={SetContainer}/>
       </Switch>
     </div>
   );
---
+++ src/pages/SetContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SetContainer(props) {
+    return (
+        <div>
+            im the set container
+        </div>
+    )
+}
