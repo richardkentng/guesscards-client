de929ee0119947e81ee83ef0124fc225c704c5a8
Richard Ng <richardkentng@gmail.com>
2021-04-07 12:36:50 -0700

    navbar conditionally renders links & has logout & redirect functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {useState} from 'react'
+import { Link, withRouter } from 'react-router-dom'
 
-export default function Navbar() {
+import { useRecoilState } from 'recoil'
+import {userState} from '../recoil/atom'
+
+function Navbar(props) {
+    const [user, setUser] = useRecoilState(userState)
+
+    const logout = () => {
+        localStorage.clear()
+        setUser(false)
+        props.history.push('/')
+    }
+
     return (
-        <nav>
+        <nav style={{backgroundColor: user ? 'rgb(163, 203, 117)' : '#ffa4a4'}}>
             <ul>
-                <li><Link to="/sets">Studysets</Link></li>
-                <li><Link to="/signup">Sign Up</Link></li>
-                <li><Link to="/login">Log In</Link></li>
+                {user ? (
+                    <>
+                    <li><Link to="/sets">STUDYSETS</Link></li>
+                    <li onClick={logout}><a href="#">({user.username}) Log Out</a></li>
+                    </>
+                ) : (
+                    <>
+                    <li><Link to="/signup">Sign Up</Link></li>
+                    <li><Link to="/login">Log In</Link></li>
+                    </>
+                )}
             </ul>
         </nav>
     )
 }
+
+
+export default withRouter(Navbar)
