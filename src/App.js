41f335c4464b25d3a1c4403295c2dbc43377e684
Richard Ng <richardkentng@gmail.com>
2021-04-07 12:33:54 -0700

    adds landing and login route. Configures toast.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 import Navbar from './components/Navbar'
 
+import Landing from './pages/Landing'
 import Signup from './pages/Signup'
+import Login from './pages/Login'
 import SetContainer from './pages/SetContainer'
+
 
 import './style.css'
 
+toast.configure()
 function App() {
   return (
     <div>
       <Navbar/>
       <Switch>
+        <Route exact path="/" component={Landing}/>
         <Route path="/signup" component={Signup}/>
+        <Route path="/login" component={Login}/>
         <Route path="/sets" component={SetContainer}/>
       </Switch>
     </div>
