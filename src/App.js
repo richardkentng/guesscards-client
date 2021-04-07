eb5d509c5ef3c2de5293466d00402c2141416916
Richard Ng <richardkentng@gmail.com>
2021-04-06 19:37:30 -0700

    imports css and navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import {Switch, Route} from 'react-router-dom'
 
+import Navbar from './components/Navbar'
+
 import Signup from './pages/Signup'
 import SetContainer from './pages/SetContainer'
+
+import './style.css'
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Switch>
         <Route path="/signup" component={Signup}/>
         <Route path="/sets" component={SetContainer}/>
