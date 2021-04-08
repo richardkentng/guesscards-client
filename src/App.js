c0cbc7a8a11e12d9f52943dd26764697b7894b2c
Richard Ng <richardkentng@gmail.com>
2021-04-08 11:25:48 -0700

    adds setShow route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar'
 import Landing from './pages/Landing'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import SetShow from './pages/SetShow'
 import SetContainer from './pages/SetContainer'
 
 
@@ -21,6 +22,7 @@ function App() {
         <Route exact path="/" component={Landing}/>
         <Route path="/signup" component={Signup}/>
         <Route path="/login" component={Login}/>
+        <Route path="/sets/:id" component={SetShow}/>
         <Route path="/sets" component={SetContainer}/>
       </Switch>
     </div>
