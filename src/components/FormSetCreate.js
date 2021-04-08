6a17dcd92d52eecb5aa08febbdfa2ba107d4f5d8
Richard Ng <richardkentng@gmail.com>
2021-04-08 11:31:17 -0700

    cadds form to create new set

---
+++ src/components/FormSetCreate.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react'
+import { toast } from 'react-toastify'
+import SetModel from '../models/set'
+
+export default function FormSetCreate(props) {
+
+    const [setName, setSetName] = useState("")
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if (!setName) return toast.warn('Please provide a name for your set!')
+        props.setCreate(setName)
+        setSetName('')
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input 
+            type="text"
+            name="name"
+            value={setName}
+            placeholder="eg: ENG 101 Vocab"
+            autoFocus={true}
+            onChange={(e) => setSetName(e.target.value)}
+            />
+            <button type="submit">Create Set</button>
+        </form>
+    )
+}
