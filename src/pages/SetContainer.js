044c86468539b18682c3b6f0f6b4647362cd2573
Richard Ng <richardkentng@gmail.com>
2021-04-07 21:02:59 -0700

    displays sets specific to user

--- src/pages/SetContainer.js
+++ src/pages/SetContainer.js
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import SetModel from '../models/set'
 export default function SetContainer(props) {
+
+    const [sets, setSets] = useState(false)
+
+    useEffect(() => {
+        SetModel.all().then(data => {
+            // console.log('from client. DATA RECEIVED from get request to domain/sets:', data.sets);
+            setSets(data.sets)
+        })
+    }, [])
+
+    let uiSets
+    if (typeof sets === 'object') {
+        uiSets = sets.map((set, idx) => {
+            return <div key={idx}>{set.name}</div>
+        })
+    }
+
+
     return (
         <div>
-            im the set container
+            {!sets ? 'loading...' : (sets.length ? uiSets : 'You have no sets.   Don\'t worry, create a  new set of cards now!')}
         </div>
     )
 }
