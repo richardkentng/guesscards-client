306262a3b3f3ded85b2b642de8bb7008f1ecab34
Richard Ng <richardkentng@gmail.com>
2021-04-08 11:41:27 -0700

    imports SetModel to allow set creation. Uses SetCard to display every set

--- src/pages/SetContainer.js
+++ src/pages/SetContainer.js
@@ -1,8 +1,21 @@
 import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+
 import SetModel from '../models/set'
-export default function SetContainer(props) {
+
+import FormSetCreate from '../components/FormSetCreate'
+import SetCard from '../components/SetCard'
+
+function SetContainer(props) {
 
     const [sets, setSets] = useState(false)
+
+    const setCreate = (name) => {
+        SetModel.create({name}).then(res => {
+            // console.log('Client received response when trying to create set: ', res);
+            setSets([...sets, res.set])
+        })
+    }
 
     useEffect(() => {
         SetModel.all().then(data => {
@@ -14,14 +27,23 @@ export default function SetContainer(props) {
     let uiSets
     if (typeof sets === 'object') {
         uiSets = sets.map((set, idx) => {
-            return <div key={idx}>{set.name}</div>
+            const numCards = set.cards.length
+            return (
+            <Link key={idx} to={`/sets/${set._id}`}>
+                <SetCard name={set.name} numCards={numCards}/>
+            </Link>
+            )
         })
     }
 
 
     return (
         <div>
+            <FormSetCreate setCreate={setCreate}/>
             {!sets ? 'loading...' : (sets.length ? uiSets : 'You have no sets.   Don\'t worry, create a  new set of cards now!')}
         </div>
     )
 }
+
+
+export default SetContainer
