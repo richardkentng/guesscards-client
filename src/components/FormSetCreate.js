7b57daed76cc555a15079b0892511c540781b0c5
Richard Ng <richardkentng@gmail.com>
2021-04-08 23:30:56 -0700

    removes unused vars

--- src/components/FormSetCreate.js
+++ src/components/FormSetCreate.js
@@ -1,6 +1,5 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import { toast } from 'react-toastify'
-import SetModel from '../models/set'
 
 export default function FormSetCreate(props) {
 
