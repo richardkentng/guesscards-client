b23984fc829dba09b55df2664eaf37eea11ed4d6
Richard Ng <richardkentng@gmail.com>
2021-04-11 11:56:19 -0700

    adds control flow for keydown enter. Pulses emty fields red.

--- src/components/FormFlashcardCreate.js
+++ src/components/FormFlashcardCreate.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { toast } from 'react-toastify'
 // import SetModel from '../models/set'
 
 class FormFlashcardCreate extends React.Component {
@@ -8,38 +7,74 @@ class FormFlashcardCreate extends React.Component {
         ques: '',
         ans: ''
     }
-    // const [ques, setQues] = useState("")
-    // const [ans, setAns] = useState("")
-    // componentDidMount() {
-    //     const formNewFcardEl = document.querySelector('.form-new-fcard')
-    //     formNewFcardEl.addEventListener('keydown', this.onEnter)
-    // }
-    // componentWillUnmount() {
-    //     const formNewFcardEl = document.querySelector('.form-new-fcard')
-    //     formNewFcardEl.removeEventListener('keydown', this.onEnter)
-    // }
-    
-    // onEnter(e) {
-    //     if (e.key === "Enter") {
-    //         e.preventDefault()
-    //         console.log('u press enter');
-    //         const inputNewQuesEl = document.querySelector('.new-ques')
-    //         const inputNewAnsEl = document.querySelector('.new-ans')
-    //     }
-    // }
 
-    onSubmit = (e) => {
+    // control flow for "keydown" of enter.  
+    // focuses fields and pulses them red based on which fields are empty.
+    componentDidMount() {
+        document.querySelector('.form-new-fcard')
+        .addEventListener('keydown', this.onEnter)
+    }
+    componentWillUnmount() {
+        document.querySelector('.form-new-fcard')
+        .removeEventListener('keydown', this.onEnter)
+    }
+    onEnter = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault()
+            const inputNewQuesEl = document.querySelector('.new-ques')
+            const inputNewAnsEl = document.querySelector('.new-ans')
+            const {ques, ans} = this.state
+            const activeEl = document.activeElement
+            // flip between empty fields for question and answer
+            if (!ques && !ans) {
+                if (activeEl === inputNewAnsEl) {
+                    return inputNewQuesEl.focus()
+                } else if (activeEl === inputNewQuesEl) {
+                    return inputNewAnsEl.focus()
+                }
+            }
+            // focus empty field.  if already focused, pulse it red as well
+            if (!ques) {
+                if (activeEl === inputNewQuesEl) this.pulseRed(inputNewQuesEl)
+                return inputNewQuesEl.focus()
+            } else if (!ans) {
+                if (activeEl === inputNewAnsEl) this.pulseRed(inputNewAnsEl)
+                return inputNewAnsEl.focus()
+            }
+            this.onSubmitCreateFcard(e)
+        }
+    }
+
+    onSubmitCreateFcard = (e) => {
         e.preventDefault()
-        if (!this.state.ques || !this.state.ans) return toast.warn('Please fill out both fields!')
+
+        // ensure fields are not empty
+        const {ques, ans} = this.state
+        const inputNewQuesEl = document.querySelector('.new-ques')
+        const inputNewAnsEl = document.querySelector('.new-ans')
+        if (!ques) {
+            this.pulseRed(inputNewQuesEl)
+            return inputNewQuesEl.focus()
+        } else if (!ans) {
+            this.pulseRed(inputNewAnsEl)
+            return inputNewAnsEl.focus()
+        }
+        
         this.props.flashcardCreate({ques: this.state.ques, ans: this.state.ans})
         this.setState({
             ques: '',
             ans: ''
         })
     }
+
+    pulseRed = (el) => {
+        el.style.backgroundColor = "pink"
+        setTimeout(() => { el.style.backgroundColor = "white"}, 100)
+    }
+    
     render() {
         return (
-            <form onSubmit={this.onSubmit} className="FlashCard form-new-fcard" style={{backgroundColor: "silver"}}>
+            <form onSubmit={this.onSubmitCreateFcard} className="FlashCard form-new-fcard" style={{backgroundColor: "silver"}}>
                 <div>
                     <input 
                     placeholder="question"
@@ -47,7 +82,7 @@ class FormFlashcardCreate extends React.Component {
                     value={this.state.ques}
                     autoFocus={true}
                     onChange={(e) => this.setState({ques: e.target.value})}
-                    className="new-ques"
+                    className="new-ques w100p"
                     />
                 </div>
 
@@ -57,11 +92,11 @@ class FormFlashcardCreate extends React.Component {
                     type="text"
                     value={this.state.ans}
                     onChange={(e) => this.setState({ans: e.target.value})}
-                    className="new-ans"
+                    className="new-ans w100p"
                     />
                 </div>
 
-                <button type="submit">Create Flashcard</button>
+                <button type="submit" className="w100p gray-72">Create Flashcard</button>
             </form>
         )
     }
