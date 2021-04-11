45cf9521b189ef4f2205f10ab06352541a501d21
Richard Ng <richardkentng@gmail.com>
2021-04-11 10:32:06 -0700

    refactored create-flashcard-form into class component

--- src/components/FormFlashcardCreate.js
+++ src/components/FormFlashcardCreate.js
@@ -1,46 +1,70 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { toast } from 'react-toastify'
 // import SetModel from '../models/set'
 
-function FormFlashcardCreate(props) {
+class FormFlashcardCreate extends React.Component {
 
-    const [ques, setQues] = useState("")
-    const [ans, setAns] = useState("")
-
-    const onSubmit = (e) => {
-        e.preventDefault()
-        if (!ques || !ans) return toast.warn('Please fill out both fields!')
-        props.flashcardCreate({ques, ans})
-        setQues('')
-        setAns('')
+    state = {
+        ques: '',
+        ans: ''
     }
+    // const [ques, setQues] = useState("")
+    // const [ans, setAns] = useState("")
+    // componentDidMount() {
+    //     const formNewFcardEl = document.querySelector('.form-new-fcard')
+    //     formNewFcardEl.addEventListener('keydown', this.onEnter)
+    // }
+    // componentWillUnmount() {
+    //     const formNewFcardEl = document.querySelector('.form-new-fcard')
+    //     formNewFcardEl.removeEventListener('keydown', this.onEnter)
+    // }
+    
+    // onEnter(e) {
+    //     if (e.key === "Enter") {
+    //         e.preventDefault()
+    //         console.log('u press enter');
+    //         const inputNewQuesEl = document.querySelector('.new-ques')
+    //         const inputNewAnsEl = document.querySelector('.new-ans')
+    //     }
+    // }
 
-    return (
-        <form onSubmit={onSubmit} className="FlashCard" style={{backgroundColor: "silver"}}>
-            <div>
-                <input 
-                type="text"
-                name="ques"
-                value={ques}
-                placeholder="question"
-                autoFocus={true}
-                onChange={(e) => setQues(e.target.value)}
-                />
-            </div>
+    onSubmit = (e) => {
+        e.preventDefault()
+        if (!this.state.ques || !this.state.ans) return toast.warn('Please fill out both fields!')
+        this.props.flashcardCreate({ques: this.state.ques, ans: this.state.ans})
+        this.setState({
+            ques: '',
+            ans: ''
+        })
+    }
+    render() {
+        return (
+            <form onSubmit={this.onSubmit} className="FlashCard form-new-fcard" style={{backgroundColor: "silver"}}>
+                <div>
+                    <input 
+                    placeholder="question"
+                    type="text"
+                    value={this.state.ques}
+                    autoFocus={true}
+                    onChange={(e) => this.setState({ques: e.target.value})}
+                    className="new-ques"
+                    />
+                </div>
 
-            <div>
-                <input 
-                type="text"
-                name="ans"
-                value={ans}
-                placeholder="answer"
-                onChange={(e) => setAns(e.target.value)}
-                />
-            </div>
+                <div>
+                    <input 
+                    placeholder="answer" 
+                    type="text"
+                    value={this.state.ans}
+                    onChange={(e) => this.setState({ans: e.target.value})}
+                    className="new-ans"
+                    />
+                </div>
 
-            <button type="submit">Create Flashcard</button>
-        </form>
-    )
+                <button type="submit">Create Flashcard</button>
+            </form>
+        )
+    }
 }
 
 export default FormFlashcardCreate
