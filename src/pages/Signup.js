cb4244d04719b9a9a19ad957b50624420a1f195e
Richard Ng <richardkentng@gmail.com>
2021-04-11 20:42:43 -0700

    adds signup validation for username & password

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,12 @@
 import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 import { toast } from 'react-toastify'
 import AuthModel from '../models/auth'
 import {useSetRecoilState} from 'recoil'
 import {userState} from '../recoil/atom'
 import signup_drawn from '../images/signup_drawn.png'
+
+
 export default function Signup(props) {
 
     const setUser = useSetRecoilState(userState)
@@ -13,6 +16,11 @@ export default function Signup(props) {
     const onSignup = (e) => {
         e.preventDefault()
         if (!username || !password) return toast.warn('Please fill out both fields!')
+        if (password.length < 8) return toast.warn('Password must be at least 8 characters long.')
+        if (password.length > 100) return toast.warn('Password may not exceed 100 characters!')
+        if (username.length > 100) return toast.warn('Username may not exceed 100 characters!')
+        if (username.split('').includes(' ')) return toast.warn('Username may not contain spaces!')
+
         AuthModel.signup({username, password}).then(res => {
             console.log('from client. res retrieved from api\'s signup route', res);
             //store token in local storage
@@ -50,7 +58,6 @@ export default function Signup(props) {
                         autoFocus={true}
                         />
                     </div>
-    
                     <div>
                         <input 
                         placeholder="enter a password" 
@@ -60,9 +67,14 @@ export default function Signup(props) {
                         onChange={(e) => setPassword(e.target.value)} 
                         />
                     </div>
+
+                    <ul className="fs12 mt5 mb10 ml10">
+                    <li>at least 8 characters long</li>
+                    </ul>
     
                     <button className="btn-signup" type="submit">Sign Me Up Right Now!</button>
                 </div>
+                    <p className="tac mt10">Already registered? <Link to="/login">Log In!</Link></p>
             </form>
         </>
     )
