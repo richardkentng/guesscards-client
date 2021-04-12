6e3c01dc8f1a7d6a5326f285a5f0acc1cacf5c79
Richard Ng <richardkentng@gmail.com>
2021-04-12 08:16:05 -0700

    adds landing page with images & steps

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,54 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import step1 from '../images/step1.png'
+import step2 from '../images/step2.png'
+import step3 from '../images/step3.png'
+import step4 from '../images/step4.png'
+import step5 from '../images/step5.png'
+import step6 from '../images/step6.png'
 export default function Landing() {
     return (
         <>
-            <h1>GUESSCARDS!</h1>
-            <p><i>The smarter way to study flashcards!</i></p>
+            <div className="Landing-header">
+                <h1>GUESSCARDS!</h1>
+                <p><i>Flashcards made easy!</i></p>
+            </div>
+
+            <div className="Landing-grid">
+                <section>
+                <p>1 create a set.</p>
+                <img src={step1} alt="step1"/>
+                </section>
+
+                <section>
+                <p>2 Create a flashcard.</p>
+                <img src={step2} alt="step2"/>
+                </section>
+
+                <section>
+                <p>3 Guess the answer!</p>
+                <img src={step3} alt="step3"/>
+                </section>
+
+                <section>
+                <p>4 Click to see your answer!</p>
+                <img src={step4} alt="step4"/>
+                </section>
+
+                <section>
+                <p>Create more and more flashcards!</p>
+                <img src={step5} alt="step5"/>
+                </section>
+
+                <section>
+                <p>And easily view all your sets!</p>
+                <img src={step6} alt="step6"/>
+                </section>
+
+                <section>
+                <p>Sound easy?  <Link to="/signup">Sign Up!</Link></p>
+                </section>
+            </div>
         </>
     )
 }
