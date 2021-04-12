0ab9c8788647618c537b01d9491165a659205943
Richard Ng <richardkentng@gmail.com>
2021-04-12 01:00:24 -0700

    Adds classes to navbar for css positioning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import AuthModel from '../models/auth'
 
 import { useRecoilState } from 'recoil'
 import {userState} from '../recoil/atom'
+import icon from '../images/icon_t.png'
 
 function Navbar(props) {
     const [user, setUser] = useRecoilState(userState)
@@ -28,16 +29,22 @@ function Navbar(props) {
     return (
         <nav className={user ? 'nav-bb-in' : 'nav-bb-out'}>
             <ul>
+                    <li className="icon">
+                        <Link to="/"><img src={icon} alt="guesscards"/></Link>
+                    </li>
                 {user ? (
                     <>
-                    <li><Link to="/sets">STUDYSETS</Link></li>
-                    <li><button onClick={logout} className="link-button">({user.username}) Log Out</button></li>
-                    <li>{user._id}</li>
+                    <li className="logout"><button onClick={logout} className="link-button"> Log Out</button></li>
+                    <li className="sets"><Link to="/sets">SETS</Link></li>
+                    <li className="gray username">{user.username}</li>
+                    {/* <li>{user._id}</li> */}
                     </>
                 ) : (
                     <>
-                    <li><Link to="/signup">Sign Up</Link></li>
-                    <li><Link to="/login">Log In</Link></li>
+                    <li className="about"><Link to="/about">About Me</Link></li>
+                    <li className="login"><Link to="/login">Log In</Link></li>
+                    <li className="signup"><Link to="/signup">Sign Up</Link></li>
+                    <li></li>
                     </>
                 )}
             </ul>
