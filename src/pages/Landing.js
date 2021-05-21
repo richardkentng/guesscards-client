253f2d8ad556c2abbcb291a81d4a550f55a93678
Richard Ng <richardkentng@gmail.com>
2021-05-20 18:51:34 -0700

    adds iframe demo video to landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -13,6 +13,8 @@ export default function Landing() {
                 <h1>GUESSCARDS!</h1>
                 <p><i>Flashcards made easy!</i></p>
             </div>
+            
+            <iframe class="iframe" width="560" height="315" src="https://www.youtube.com/embed/eCi1TkFhSo4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
             <div className="Landing-grid">
                 <section>
