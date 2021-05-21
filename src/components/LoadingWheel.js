6bdf4e0177ba66b7d9e99b654b50dc058a5e99d6
Richard Ng <richardkentng@gmail.com>
2021-05-21 09:57:40 -0700

    creates LoadingWheel component

---
+++ src/components/LoadingWheel.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function LoadingWheel() {
+    return <img src="https://i.imgur.com/llF5iyg.gif" className="loading-wheel" alt="loading wheel"/>
+}
+
+export default LoadingWheel
