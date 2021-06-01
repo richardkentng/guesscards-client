0147395a4d54057cdff7a23c16ae35bd72ae94b3
Richard Ng <richardkentng@gmail.com>
2021-06-01 16:14:39 -0700

    clicking any button.btn-sort will ignore button.btn-sort-mark, which appears bif > 0 cards are marked

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,11 +1,8 @@
 import React, {useEffect} from 'react'
 import SearchBar from './SearchBar'
+import { toast } from 'react-toastify'
 
 function Options(props) {
-
-    function sortByNewFirst() { props.sortByCreatedAt('desc') }
-    function sortByOldFirst() { props.sortByCreatedAt('asc') }
-
 
     useEffect(() => {
         document.querySelector('.btn-sort-new').disabled = true
@@ -15,23 +12,39 @@ function Options(props) {
         }
     }, [])
 
-    function toggleDisabled(target) {
-        //remember sort in localStorage
-        const classList = target.getAttribute('class').split(' ')
-        const clas = classList.find(c => c.includes('btn-sort-'))
-        localStorage.setItem('sort', clas)
 
-        //toggleDisabled
+    function toggleDisabled(e) {
+        //ensure that target is a button (because buttons have a class that describes their sort type)
+        let btnClicked = e.target
+        while (btnClicked.localName !== 'button') {
+            btnClicked = btnClicked.parentElement
+        }
+
+        //set 'sort' in localStorage (exclude marked)
+        if (!btnClicked.classList.contains('btn-sort-mark')) {
+            let btnSortClass
+            btnClicked.classList.forEach(c => {
+                if (c.includes('btn-sort-')) btnSortClass = c
+            })
+            if (!btnSortClass) return toast.error(`Could not find class with "btn-sort-" from clicked target.
+            LocalStorage did not store a value for "sort"`)
+            localStorage.setItem('sort', btnSortClass)
+        }
+
+        //disable clicked button (exluding button.btn-sort-mark)
+        //enable other buttons
         const btns = document.querySelectorAll('.btn-sort')
         btns.forEach(btn => {
-            if (btn === target) {
-                if (btn.classList.contains('btn-sort-rand')) btn.style.opacity = '0.5'
-                else { btn.disabled = true }
+            //ignore btn-sort-mark button
+            if (!btn.classList.contains('btn-sort-mark')) {
+                if (btn === btnClicked) {
+                    if (btn.classList.contains('btn-sort-rand')) btn.style.opacity = '0.5'
+                    else { btn.disabled = true }
+                }
+                else if (btn.classList.contains('btn-sort-rand')) btn.style.opacity = '1'
+                else { btn.disabled = false }
             }
-            else if (btn.classList.contains('btn-sort-rand')) btn.style.opacity = '1'
-            else { btn.disabled = false }
         })
-        if (!props.numMarked) document.querySelector('.btn-sort-mark').disabled = true
     }
 
     return (
@@ -41,39 +54,42 @@ function Options(props) {
             <button 
                 onClick={(e) => {
                     props.sortByRandom()
-                    toggleDisabled(e.target)
+                    toggleDisabled(e)
                 }}
-                className="btn-sort btn-sort-rand gray-72" 
+                className="btn-sort btn-sort-rand" 
                 >
-                Random
+                Rand
             </button>
             <button
                 onClick={(e) => {
-                    sortByNewFirst()
-                    toggleDisabled(e.target)
+                    props.sortByCreatedAt('desc')
+                    toggleDisabled(e)
                 }}
-                className="btn-sort btn-sort-new gray-72"
+                className="btn-sort btn-sort-new"
                 >
-                Newest
+                New
             </button>
             <button
                 onClick={(e) => {
-                    sortByOldFirst()
-                    toggleDisabled(e.target)
+                    props.sortByCreatedAt('asc')
+                    toggleDisabled(e)
                 }}
-                className="btn-sort btn-sort-old gray-72" 
+                className="btn-sort btn-sort-old" 
                 >
-                Oldest
+                Old
             </button>
             <button
                 onClick={(e) => {
                     props.sortByMarked()
-                    toggleDisabled(e.target)
+                    toggleDisabled(e)
                 }}
-                className="btn-sort btn-sort-mark gray-72"
-                disabled={ props.numMarked ? false : true }
+                className="btn-sort btn-sort-mark"
+                style={{display: props.numMarked ? 'inline' : 'none'}}
                 >
-                Marked <span className="red-num-marked">{props.numMarked > 0 ? props.numMarked : ''}</span>
+                <span className="text-marked-num">
+                    <span>Marked</span>
+                    <span className="red-num-marked">&nbsp;{props.numMarked > 0 ? props.numMarked : ''} </span>
+                </span>
             </button>
             <SearchBar 
             onSubmitFcardSearch={props.onSubmitFcardSearch}
