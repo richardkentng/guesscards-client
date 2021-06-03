4a01bcf6785088f8d81581ff3c27672904d81dc2
Richard Ng <richardkentng@gmail.com>
2021-06-03 14:27:58 -0700

    aadds png image to scroll-to-top button

--- src/components/OptionsBottom.js
+++ src/components/OptionsBottom.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import colors from '../partials/colors'
+import img_src_arrow_up from '../images/arrow_up_20.png'
 
 function OptionsBottom(props) {
 
@@ -25,7 +26,9 @@ function OptionsBottom(props) {
                 Hide {props.numAnswersShown} Answers
             </button>
 
-            <a href="#navbar" className="btn-scroll-to-top">_/\_</a>
+            <a href="#navbar" className="a-scroll-to-top">
+                <img src={img_src_arrow_up} alt="arrow up"/>
+            </a>
         </div>
     )
 }
