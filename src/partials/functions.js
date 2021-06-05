6580010809ce26434aee3410bc7e9e86877940de
Richard Ng <richardkentng@gmail.com>
2021-06-04 23:58:23 -0700

    adds partial 'functions' with 'handleAuthErrorsWithToasts' function

---
+++ src/partials/functions.js
@@ -0,0 +1,11 @@
+const { toast } = require("react-toastify")
+
+function handleAuthErrorsWithToasts(res) {
+    if ('msg' in res && res.msg === "token does not exist in middleware") toast.warn('You must be logged in to access that route!')
+    else if ('err' in res && res.err.name === "TokenExpiredError") toast.warn('Your session expired. Please log in again.')
+    else toast.error('An error occured... The server did not respond with the proper data. Try logging in again.')
+}
+
+module.exports = {
+    handleAuthErrorsWithToasts
+}
