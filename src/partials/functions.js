9d2635b7b3f992f91ac87b81f232df309be3201b
Richard Ng <richardkentng@gmail.com>
2021-06-06 01:11:02 -0700

    configures handleAuthErrorsWithToasts to display res.msg

--- src/partials/functions.js
+++ src/partials/functions.js
@@ -3,6 +3,7 @@ const { toast } = require("react-toastify")
 function handleAuthErrorsWithToasts(res) {
     if ('msg' in res && res.msg === "token does not exist in middleware") toast.warn('You must be logged in to access that route!')
     else if ('err' in res && res.err.name === "TokenExpiredError") toast.warn('Your session expired. Please log in again.')
+    else if ('msg' in res) toast.error(res.msg)
     else toast.error('An error occured... The server did not respond with the proper data. Try logging in again.')
 }
 
